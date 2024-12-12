035ca79c1f62d6ee3f1cc0127bb0ec508271f5e1
DaviEmmanuel04 <juninhors1303@gmail.com>
2024-12-12 18:45:56 -0300

    alterações nos apps 2 e 3

--- app-2/App.js
+++ app-2/App.js
@@ -2,48 +2,57 @@ import React, { useState } from 'react';
 import { Button, SafeAreaView, Switch, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [peso, setPeso] = useState('')
+  const [displayImc, setDisplayImc] = useState(<></>)
+  const [altura, setAltura] = useState('')
+
+  function imcCalc(peso, altura) {
+    const alturaNum = parseFloat(altura);
+    const pesoNum = parseFloat(peso);
+
+    const imc = pesoNum / (alturaNum * alturaNum);
+
+    let categoria = '';
+    if (imc < 18.5) {
+      categoria = 'Abaixo de Peso';
+    } else if (imc >= 18.5 && imc < 25) {
+      categoria = 'Peso Normal';
+    } else if (imc >= 25 && imc < 30) {
+      categoria = 'Sobrepeso';
+    } else if (imc >= 30 && imc < 35) {
+      categoria = 'Obesidade Grau I';
+    } else if (imc >= 35 && imc < 40) {
+      categoria = 'Obesidade Grau II';
+    } else if (imc >= 40) {
+      categoria = 'Obesidade Mórbida';
+    }
+
+    setDisplayImc(
+      <View>
+        <Text>Com um peso de {peso} kg</Text>
+        <Text>e uma altura de {altura} m</Text>
+        <Text>Seu IMC é: {imc.toFixed(2)}</Text>
+        <Text>Você está classificado como: {categoria}</Text>
+      </View>
+    );
+  }
+
+  function submitImc(){
+    if (peso.trim() === '' || altura.trim() === '') {
+      setDisplayImc(<Text>digite algo</Text>);
+    } else if (isNaN(peso) || isNaN(altura)) {
+      setDisplayImc(<Text>digite um número válido</Text>);
+    } else {
+      imcCalc(peso, altura);
+    }
+  }
+
   return (
     <SafeAreaView>
-      <Welcome />
-      <Input />
-      <Botoes texto="Clique aqui" />
+      <TextInput placeholder='digite seu peso' value={peso} onChangeText={setPeso}></TextInput>
+      <TextInput placeholder='digite sua altura' value={altura} onChangeText={setAltura}></TextInput>
+      <Button onPress={submitImc} title='calcular imc'></Button>
+      <>{displayImc}</>
     </SafeAreaView>
   );
-}
-
-function onPressButton() {
-  alert('Você clicou!');
-}
-
-function Botoes(props) {
-
-  const [ligado, setLigado] = useState(false)
-
-  return (
-    <View>
-      <Button onPress={onPressButton} title={props.texto}></Button>
-      <Switch onValueChange={() => setLigado(!ligado)} value={ligado}></Switch>
-    </View>
-  )
-}
-
-function Input() {
-
-  const [text, setText] = useState('')
-
-  return (
-    <View>
-      <TextInput style={{ height: 40 }} placeholder="Digite aqui seu nome" value={text} onChangeText={(text) => setText(text)}>
-      </TextInput>
-      <Text>{text}</Text>
-    </View>
-  )
-}
-
-function Welcome() {
-  return (
-    <View>
-      <Text>Bem vindo!</Text>
-    </View>
-  )
 }
--- app-3/App.js
+++ app-3/App.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import { SafeAreaView, View, Text, SectionList } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, FlatList, TextInput, Button } from 'react-native';
 
 const DATA = [
   {
     title: "Pratos principais",
     data: ["Pizza", "Burger", "Risoto"]
-  },
-  {
-    title: "Entradas",
-    data: ["Batata Frita", "Onion Rings"]
-  },
-  {
-    title: "Sobremesas",
-    data: ["Sorvete de Creme", "Petit Gateau"]
   }
 ]
 
 function Item(props) {
+
   return (
     <View style={{ height: 60, backgroundColor: '#AFC', padding: 10, margin: 10 }}>
       <Text>{props.title}</Text>
@@ -25,14 +18,44 @@ function Item(props) {
 }
 
 export default function App() {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [data, setData] = useState('');
+  const [tarefa, setTarefa] = useState([]);
+
+  function addTarefa(){
+    if (titulo && descricao && data) {
+      const novaTarefa = {
+        id: Math.random().toString(),
+        titulo,
+        descricao,
+        data
+      };
+      setTarefa((tarefa) => [...tarefa, novaTarefa]);
+      setTitulo('');
+      setDescricao('');
+      setData('');
+    } else {
+      alert('Por favor, preencha todos os campos!');
+    }
+  }
+
   return (
     <SafeAreaView>
-      <SectionList
-        style={{ margin: 8 }}
-        sections={DATA}
-        renderItem={it => <Item title={it.item} />}
-        renderSectionHeader={(item) => <Text style={{ fontSize: 20 }}>{item.section.title}</Text>}
-        keyExtractor={(item, index) => item + index}
+      <TextInput placeholder='título' value={titulo} onChangeText={setTitulo}></TextInput>
+      <TextInput placeholder='descrição' value={descricao} onChangeText={setDescricao}></TextInput>
+      <TextInput placeholder='data' value={data} onChangeText={setData}></TextInput>
+      <Button title='adicionar tarefa' onPress={addTarefa}></Button>
+      <FlatList
+        data={tarefa}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.titulo}</Text>
+            <Text>{item.descricao}</Text>
+            <Text>{item.data}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.id}
       />
     </SafeAreaView>
   );
